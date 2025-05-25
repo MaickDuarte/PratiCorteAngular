151a91a8ff68be61a5865da3b679c95f76f5f0d4
Maick Duarte <maickduarte.2@hotmail.com>
2025-05-25 12:01:01 -0300

    componentizando tela de login e importando form

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-login-layout',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './default-login-layout.component.scss'
 })
 export class DefaultLoginLayoutComponent {
-
+  @Input() title: string = ""
+  @Input() loginButtonText: string = ""
+  @Input() forgotPasswordText: string = ""
+  @Input() registerButtonText: string = ""
+  @Input() notClientYetText: string = ""
 }
