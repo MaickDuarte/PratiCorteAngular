56a8449a1facef2ccb9ee78f9268908026021085
Maick Duarte <maickduarte.2@hotmail.com>
2025-05-25 13:16:00 -0300

    adicionando fonte, reestilizando e reformulando value do form

--- src/app/components/login-input/login-input.component.ts
+++ src/app/components/login-input/login-input.component.ts
@@ -1,20 +1,51 @@
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-type InputType = 'text' | 'email' | 'password'
+type InputType = 'text' | 'email' | 'password';
 
 @Component({
   selector: 'app-login-input',
-  imports: [
-    ReactiveFormsModule
-  ],
   templateUrl: './login-input.component.html',
-  styleUrl: './login-input.component.scss'
+  styleUrl: './login-input.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => LoginInputComponent),
+      multi: true,
+    }
+  ],
+  standalone: true
 })
-export class LoginInputComponent {
-  @Input() formName: string = ''
+export class LoginInputComponent implements ControlValueAccessor {
   @Input() type: InputType = 'text'
   @Input() label: string = ''
   @Input() placeholder: string = ''
+  @Input() inputName: string = ''
+
+  value: string = ''
+
+  private onChange: (value: string) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  onInput(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.value = target.value;
+    this.onChange(this.value);
+  }
+
+  onBlur(): void {
+    this.onTouched();
+  }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 }
