e893ed345f1f5d068c428311ad0e8da87c371c3d
Maick Duarte <maickduarte.2@hotmail.com>
2025-05-25 20:50:35 -0300

    toast e validação de pré login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,13 +3,16 @@ import { DefaultLoginLayoutComponent } from '../../components/default-login-layo
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { LoginInputComponent } from '../../components/login-input/login-input.component';
 import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+import { isEmail, isPassword } from '../../shared/utils';
 
 @Component({
   selector: 'app-login',
   imports: [
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
-    LoginInputComponent
+    LoginInputComponent,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -18,7 +21,9 @@ export class LoginComponent {
   loginForm!: FormGroup;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private loginService: LoginService,
+    private toastrService: ToastrService
   ) { 
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -27,7 +32,22 @@ export class LoginComponent {
   }
 
   submit() {
-    console.log(this.loginForm.value)
+    if (!isEmail(this.loginForm.value.email)) {
+      this.toastrService.error('Email inválido')
+      return
+    }
+    if (!isPassword(this.loginForm.value.password)) {
+      this.toastrService.error('Senha inválida')
+      return
+    }
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => {
+        this.toastrService.success('Login Successful')
+      },
+      error: (err) => {
+        this.toastrService.error('Erro no login, tente novamente!')
+      }
+    })
   }
 
   navigate() {
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,11 @@ export class LoginService {
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string) {
-    return this.http.post<any>('http://localhost:3000/login', { email, password })
+    return this.http.post<LoginResponse>('http://localhost:3000/login', { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem('authToken', value.token)
+        sessionStorage.setItem('username', value.name)
+      })
+    )
   }
 }
